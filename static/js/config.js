b9e32854d1469a969cdf41c04671f8099d9cbe4e
huyongdi <huyongdi@uworks.cc>
2016-03-30 13:41:31 +0800

    初步搭建 待会再看

---
+++ static/js/config.js
@@ -0,0 +1,22 @@
+seajs.config({
+  // 别名配置
+  alias: {
+    // 通用框架模块
+    "jquery": "jquery/1.7.2/jquery",
+
+    // 当前项目自定义模块
+    "CommonOperation": "../js/CommonOperation",
+
+  },
+  // 映射配置
+  map: [
+    ['.css', '.css'],
+    ['.js', '.js' ]
+  ]
+});
+
+
+
+
+
+
