fd56bab3cc08d9662a8d216148353a1ae00a826d
huyongdi <huyongdi@uworks.cc>
2016-08-16 14:14:40 +0800

    测试google统计

--- static/js/bai_du.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?370bf880e6ce059fa27025a81e6aaa5d";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
---
+++ static/js/data_statistics.js
@@ -0,0 +1,16 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?370bf880e6ce059fa27025a81e6aaa5d";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','analytics.js','ga');
+
+ga('create', 'UA-82629607-1', 'auto');
+ga('send', 'pageview');
+
